b316df2803edbda19fc7e6667e24881d315fc90f
Edgar Soto <159398477+Soto1706@users.noreply.github.com>
2026-09-25 15:17:51 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const marker = document.querySelector('#hiro-marker');
   const btnPortfolio = document.querySelector('#btn-portfolio');
 
-  // Eventos de detección del marcador Hiro
   marker.addEventListener('markerFound', () => {
     console.log('Marcador Hiro detectado correctamente.');
   });
@@ -11,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Marcador fuera del área de visión.');
   });
 
-  // Abrir enlace al hacer clic en el botón 3D
+  // Abrir tu portafolio al hacer clic en el botón 3D
   if (btnPortfolio) {
     btnPortfolio.addEventListener('click', () => {
       window.open('https://github.com/Soto1706', '_blank');
